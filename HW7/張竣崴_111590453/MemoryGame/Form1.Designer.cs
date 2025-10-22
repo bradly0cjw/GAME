862bfeb61f8360611368bcd55dc68c11d16f22d0
bradly0cjw <57744542+bradly0cjw@users.noreply.github.com>
2025-10-22 17:08:42 +0800

    add HW7 -3

---
+++ HW7/張竣崴_111590453/MemoryGame/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace MemoryGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel_tool = new System.Windows.Forms.Panel();
+            this.panel_board = new System.Windows.Forms.Panel();
+            this.panel_tool.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 61);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Memory Game";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel_tool
+            // 
+            this.panel_tool.Controls.Add(this.button1);
+            this.panel_tool.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel_tool.Location = new System.Drawing.Point(0, 0);
+            this.panel_tool.Name = "panel_tool";
+            this.panel_tool.Size = new System.Drawing.Size(800, 88);
+            this.panel_tool.TabIndex = 1;
+            // 
+            // panel_board
+            // 
+            this.panel_board.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel_board.Location = new System.Drawing.Point(0, 88);
+            this.panel_board.Name = "panel_board";
+            this.panel_board.Size = new System.Drawing.Size(800, 362);
+            this.panel_board.TabIndex = 2;
+            this.panel_board.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel_board_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel_board);
+            this.Controls.Add(this.panel_tool);
+            this.Name = "Form1";
+            this.Text = "111590453 張竣崴";
+            this.panel_tool.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel_tool;
+        private System.Windows.Forms.Panel panel_board;
+    }
+}
+
---
+++ HW7/張竣崴_111590453/MemoryGame/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Management.Instrumentation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MemoryGame
+{
+    public partial class Form1 : Form
+    {
+        //load images
+        int card_width = 71;
+        int card_height = 96;
+        int cards_in_row = 5;
+        int cards_in_col = 4;
+        int gap = 3;
+        const int card_back_index = 55;
+        Image img;
+        int[] cards;
+        int[] poker;
+        int[] flippedCard;
+        float scale;
+        int prev_card_index;
+        public Form1()
+        {
+            InitializeComponent();
+            img = Properties.Resources.poker;
+            this.panel_board.Paint += new PaintEventHandler(this.panel_board_Paint);
+            typeof(Panel).InvokeMember("DoubleBuffered", System.Reflection.BindingFlags.SetProperty | System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic, null, panel_board, new object[] { true });
+        }
+
+        public int[] shuffelCards()
+        {
+            Random rand = new Random();
+            int[] poker = new int[52];
+            for (int i = 0; i < 52; i++)
+            {
+                poker[i] = i;
+            }
+            for (int i = 0; i < 52; i++)
+            {
+                int j = rand.Next(52);
+                int temp = poker[i];
+                poker[i] = poker[j];
+                poker[j] = temp;
+            }
+            return poker;
+        }
+
+        public int[] pickcards(int[] poker, int total)
+        {
+            // get from first total/2 card from poker and make pairs
+            int[] cards = new int[total];
+            for (int i = 0; i < total / 2; i++)
+            {
+                cards[i] = poker[i];
+                cards[i + total / 2] = poker[i];
+            }
+            // shuffle cards
+            Random rand = new Random();
+            for (int i = 0; i < total; i++)
+            {
+                int j = rand.Next(total);
+                int temp = cards[i];
+                cards[i] = cards[j];
+                cards[j] = temp;
+            }
+            return cards;
+        }
+        
+        public void drawCardsbyFlipState(Graphics g , float scale)
+        {
+            // Draw card front if flippedCard is 1, otherwise draw back
+           
+            for (int i = 0; i < cards_in_col; i++)
+            {
+                for (int j = 0; j < cards_in_row; j++)
+                {
+                    int index = i * cards_in_row + j;
+                    int card_index;
+                    if (flippedCard[index] == 1)
+                    {
+                        card_index = cards[index];
+                    }
+                    else
+                    {
+                        card_index = card_back_index;
+                    }
+                    int x = card_index / 13;
+                    int y = card_index % 13;
+                    RectangleF destRect = new RectangleF(
+                        j * (card_width * scale + gap),
+                        i * (card_height * scale + gap),
+                        card_width * scale,
+                        card_height * scale);
+                    Rectangle srcRect = new Rectangle(y * card_width, x * card_height, card_width, card_height);
+                    g.DrawImage(img, destRect, srcRect, GraphicsUnit.Pixel);
+                }
+            }
+
+        }
+
+        public void initFlippedCard(int total)
+        {
+            flippedCard = new int[total];
+            for (int i = 0; i < total; i++)
+            {
+                flippedCard[i] = 0;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // calculate scale to fit cards in panel
+            scale = Math.Min(
+                (float)(panel_board.Width - (cards_in_row - 1) * gap) / (cards_in_row * card_width), 
+                (float)(panel_board.Height - (cards_in_col - 1) * gap) / (cards_in_col * card_height));
+            // draw cards
+            int total_cards = cards_in_row * cards_in_col;
+            initFlippedCard(total_cards);
+            poker = shuffelCards();
+            cards = pickcards(poker, total_cards);
+            panel_board.Invalidate();
+
+        }
+
+        private void panel_board_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (cards == null) return;
+            //get clicked card index
+            int col = (int)(e.X / (card_width * scale + gap));
+            int row = (int)(e.Y / (card_height * scale + gap));
+            // check if click out of bounds
+            if (col >= cards_in_row || row >= cards_in_col)
+            {
+                return;
+            }
+
+            int index = row * cards_in_row + col;
+            int total_cards = cards_in_row * cards_in_col;
+            if (index >= 0 && index < total_cards)
+            {
+                flippedCard[index] = flippedCard[index] == 1 ? 0 : 1;
+                panel_board.Invalidate();
+            }
+        }
+
+        private void panel_board_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.Clear(Color.Gray);
+            if (cards != null)
+            {
+                drawCardsbyFlipState(e.Graphics, scale);
+            }
+        }
+    }
+}
